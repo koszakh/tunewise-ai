5dcf0587c260647c88a5e888ea7cd3d96497be76
Konstantin Zakharov <konstantizaharov@gmail.com>
2026-07-16 17:01:27 +0300

    JWT authentication implemented

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # По умолчанию запускаем FastAPI, но этот же образ мы переиспользуем для Celery
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ src/dependencies.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+import jwt
+
+from src.database import get_db
+from src.config import settings
+from src.models import User
+
+# Указываем URL, куда Swagger будет отправлять логин и пароль для получения токена
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось проверить учетные данные (токен недействителен)",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # Расшифровываем токен
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    # Ищем пользователя в БД
+    query = select(User).where(User.username == username)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise credentials_exception
+
+    return user
